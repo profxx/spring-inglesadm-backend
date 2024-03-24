97d701ee4ef1309f29712dd0d20d11073650e386
Alexandre Pimentel <acpimentel@gmail.com>
2024-03-24 15:42:19 -0300

    Base do Projeto

---
+++ src/main/java/br/com/profxx/inglesadm/InglesadmApplication.java
@@ -0,0 +1,13 @@
+package br.com.profxx.inglesadm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InglesadmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InglesadmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/profxx/inglesadm/InglesadmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.profxx.inglesadm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InglesadmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
